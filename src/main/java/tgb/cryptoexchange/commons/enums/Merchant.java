aa3bb6d7f8fff081dd37b42cd9a4f31c92899b78
GrewGh <grew.gh@mail.ru>
2026-05-30 12:06:23 +0300

    Доп мерчанты Deora

--- src/main/java/tgb/cryptoexchange/commons/enums/Merchant.java
+++ src/main/java/tgb/cryptoexchange/commons/enums/Merchant.java
@@ -61,6 +61,8 @@ public enum Merchant {
     ASGARD_HIGH_CHECK("Asgard High Check"),
     Z_PAY("Z-PAY"),
     DEORA("Deora"),
+    DEORA_SIM("Deora Sim"),
+    DEORA_LOW_CHECK("Deora Low Check"),
     ECLIPSE_GATE("EclipseGate"),
     ECLIPSE_GATE_HIGH_CHECK("EclipseGate High Check"),
     ECLIPSE_GATE_LOW_CHECK("EclipseGate Low Check"),
