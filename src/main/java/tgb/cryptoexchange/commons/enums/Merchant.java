6d8432700d6c310ae09c736d924e76259a355c5c
vdovin <V.Vdovin@isida.by>
2026-06-25 17:24:17 +0300

    Удален дефольный BucksPay

--- src/main/java/tgb/cryptoexchange/commons/enums/Merchant.java
+++ src/main/java/tgb/cryptoexchange/commons/enums/Merchant.java
@@ -85,7 +85,6 @@ public enum Merchant {
     BASE_51_LOW_CHECK("Base 51 Low Check"),
     BASE_51_HIGH_CHECK("Base 51 High Check"),
     PRISMA_PAY("Prisma Pay"),
-    BUCKS_PAY("Bucks Pay"),
     BUCKS_PAY_SIM("Bucks Pay Sim"),
     BUCKS_PAY_HIGH_CHECK("Bucks Pay High Check"),
     MANY_PAY("Many Pay"),
