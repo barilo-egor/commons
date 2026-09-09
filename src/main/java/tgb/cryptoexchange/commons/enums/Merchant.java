cdfd8283bb7193f00c72852bcc16dc2dc6865be7
vdovin <V.Vdovin@isida.by>
2026-09-09 09:40:20 +0300

    мерчант BayPay

--- src/main/java/tgb/cryptoexchange/commons/enums/Merchant.java
+++ src/main/java/tgb/cryptoexchange/commons/enums/Merchant.java
@@ -88,7 +88,8 @@ public enum Merchant {
     TRON_EX_SIM("TronEx SIM"),
     TRON_EX_BT("TronEx BT"),
     TRON_EX_QR("TronEx QR"),
-    TRON_EX_PDF("TronEx PDF")
+    TRON_EX_PDF("TronEx PDF"),
+    BAY_PAY("BayPay")
     ;
 
     private final String displayName;
